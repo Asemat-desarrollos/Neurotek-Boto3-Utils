32034f8355036bda0c2d43112031d7ea5b3a89de
Adrian Carreno <adriancarreno.d@gmail.com>
2024-08-13 21:29:18 -0400

    chore: Bump version to 1.2.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = \
 
 setup_kwargs = {
     'name': 'boto3-utils',
-    'version': '1.1.2',
+    'version': '1.2.0',
     'description': 'Utilidades para trabajar con Boto3',
     'long_description': '# Boto3 Utils\n\n> [!IMPORTANT]  \n> No se recomienda hacer _push_ directamente a la rama `main`. Existe un flujo de trabajo para contribuir a este proyecto, que testea y valida los cambios antes de ser integrados en la rama principal. Por favor, sigue las instrucciones de [Contribución](#contribución) para contribuir a este proyecto.\n\nEste repositorio contiene utilidades para trabajar con Boto3. Está pensado para ser publicado como un paquete de Python, alojado y publicado en GitHub.\n\n## Instalación de dependencias de desarrollo\n\n```bash\npoetry install --with dev,test\n```\n\n## Build\n\nSolo instala [poetry](https://python-poetry.org/docs/) y ejecuta:\n\n```bash\npoetry build\n```\n\n## Instalación del paquete\n\nPara instalar el paquete recién creado, simplemente ejecuta:\n\n```bash\npoetry install\n```\n\nDesde la raíz del proyecto.\n\n## Contribución\n\nEste proyecto está configurado para ejecutar pruebas en GitHub Actions. Puedes ver el estado de las pruebas en la pestaña "Actions" de este repositorio.\n\nSi desea contribuir a este proyecto, crea un Pull Request con tus cambios. Asegúrate de que todos los jobs de GitHub Actions pasen antes de solicitar la revisión de tu Pull Request.\n\n> ![IMPORTANTE]\n> Antes de publicar una nueva versión, asegúrate de ejecutar `poetry2setup > setup.py` para actualizar el archivo `setup.py` con la información de la versión actualizada.\n',
     'author': 'Adrian Carreno',
